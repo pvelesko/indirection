789295ddef710382cbe67159d5248cc7c5b537e7
Paulius Velesko <paulius.velesko@intel.com>
2019-11-12 15:46:56 +0000

    test.py continous dump

--- test.py
+++ test.py
@@ -5,9 +5,10 @@ import subprocess
 
 NUM_THREADS=[1, 2, 4]
 # 1MB, 2MB, 10MB, 100MB
-SIZE = [25000]
+#SIZE = [25000]
 #SIZE = [25000, 50000, 250000, 250000]
-FILL = [i * 0.1 for i in range(0, 10)]
+SIZE_IT = [ (25000, 500), (50000, 100), (250000, 50), (2500000, 10) ]
+FILL = [round(i * 0.1, 2) for i in range(0, 11)]
 
 def parse_test(s):
   test = s.split("\n")
@@ -25,12 +26,14 @@ def parse_test(s):
   return [t1, t2, t3, t4]
 
 e = dict(os.environ)
-
 test_run = []
-for size in SIZE:
+f = open("timing.txt", "w+")
+f.write("")
+f.close()
+for i in range(len(SIZE_IT)):
   for num_t in NUM_THREADS:
     for fill in FILL:
-      it = 10
+      size, it = SIZE_IT[i]
       cmd = ["./intel", str(size), str(it), str(fill)]
       e['OMP_NUM_THREADS'] = str(num_t)
       p = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, env=e, universal_newlines=True)
@@ -41,5 +44,7 @@ for size in SIZE:
       dat = [num_t, size, it, fill] + res
       test_run.append(dat)
       print(dat)
+      with open("timing.txt", "a") as f:
+        f.writelines(str(dat).strip("[]") + "\n")
 
 print(test_run)
